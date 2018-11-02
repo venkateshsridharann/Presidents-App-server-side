22051dc00c752bed01b4213f11dd0c2fd37ab57d
Venkatesh Sridharan <vs498@njit.edu>
2018-11-02 16:40:06 -0400

    Cors enabled

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import db from './db/db';
 import router from './routes/presroutes.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
